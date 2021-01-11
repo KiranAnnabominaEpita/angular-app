3c28784beb3140c79a79276af37d9031d794d8c1
KiranAnnabominaEpita <kiran.annabomina@epita.fr>
2021-01-11 16:03:10 +0100

    main - Practice.

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  username= '';
 
   constructor() { 
     setTimeout(() => {
@@ -27,11 +28,15 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name of the server is ' +this.serverName ;
   }
 
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  } 
+  
+  resetButton() {
+    this.username = this.username;
   }
 
 }
